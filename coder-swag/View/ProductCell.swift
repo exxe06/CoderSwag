77807712d6cc6838cdd6c0b63e41baa5c8d1be64
exxe <exxe06@MarcTerencesMBP.fritz.box>
2018-10-26 12:48:08 +0200

    third lesson finished

---
+++ coder-swag/View/ProductCell.swift
@@ -0,0 +1,15 @@
+//
+//  ProductCell.swift
+//  coder-swag
+//
+//  Created by exxe on 26.10.18.
+//  Copyright © 2018 exxe. All rights reserved.
+//
+
+import UIKit
+
+class ProductCell: UICollectionViewCell {
+    @IBOutlet weak var productImage: UIImageView!
+    @IBOutlet weak var productTitle: UILabel!
+    @IBOutlet weak var productPrice: UILabel!
+}
